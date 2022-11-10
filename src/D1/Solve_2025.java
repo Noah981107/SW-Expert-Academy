821edf447d962283013c2a81050f6aaeac18600f
Noah <shgustmd6865@naver.com>
2022-11-11 00:22:23 +0900

    D1 - N줄덧셈

---
+++ src/D1/Solve_2025.java
@@ -0,0 +1,17 @@
+package D1;
+
+import java.io.*;
+
+class Solve_2025 {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
