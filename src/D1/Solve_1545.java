71184e39489055bf36e14324536258b3843e3023
Noah <shgustmd6865@naver.com>
2022-11-10 23:22:44 +0900

    Feat : D1 - 1545.거꾸로 출력해 보아요

---
+++ src/D1/Solve_1545.java
@@ -0,0 +1,15 @@
+package D1;
+
+import java.util.Scanner;
+
+class Solve_1545 {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = T; test_case >= 0; test_case--) {
+            System.out.print(test_case + " ");
+        }
+    }
+}
