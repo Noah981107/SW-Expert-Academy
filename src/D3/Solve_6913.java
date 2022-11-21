6d9e9cb38163a83406b462bf7c4a83db11d67a11
Noah <shgustmd6865@naver.com>
2022-11-21 15:32:53 +0900

    D3 - 동철이의 프로그래밍 대회

---
+++ src/D3/Solve_6913.java
@@ -0,0 +1,45 @@
+package D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solve_6913 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int testCase=1; testCase<=T; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            int correctProblemMax = 0;
+            int sameOnePrizeNum = 0;
+
+            for (int i=0; i<N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int count = 0;
+                while (st.hasMoreTokens()) {
+                    int now = Integer.parseInt(st.nextToken());
+                    if (now == 1)
+                        count++;
+                }
+
+                if (correctProblemMax < count) {
+                    correctProblemMax = count;
+                    sameOnePrizeNum = 1;
+                }
+                else if (correctProblemMax == count) {
+                    sameOnePrizeNum++;
+                }
+            }
+
+            sb.append("#" + testCase + " ").append(sameOnePrizeNum + " ").append(correctProblemMax + "\n");
+        }
+        System.out.print(sb);
+    }
+}
